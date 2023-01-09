4308171a929682dbf644ee5274c36153c972f243
durkacper <dur.kacper@gmail.com>
2023-01-09 22:36:57 +0100

    Framework Part 5

---
+++ src/test/java/TestComponents/Listeners.java
@@ -0,0 +1,59 @@
+package TestComponents;
+
+import NOVATechnology.Resources.ExtentReporterNG;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+
+    ExtentTest test;
+    ExtentReports extentReports = ExtentReporterNG.getReportObject();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extentReports.createTest(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        //prInt the message of error
+        test.fail(result.getThrowable());
+
+        //screenshots
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        ITestListener.super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        ITestListener.super.onStart(context);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ITestListener.super.onFinish(context);
+    }
+}
